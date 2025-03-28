7ffe9ff507b01db01c27069ef3ad69a4a037b78f
un48ures <felix@letterfix.de>
2025-03-28 19:08:57 +0100

    Some refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,3 @@
-#include <Arduino.h>
-// #include <WiFi.h>
-#include <WiFiUdp.h>
 #include <ArtnetWifi.h>
 #include <FastLED.h>
 #include <esp_adc_cal.h>
@@ -19,6 +16,7 @@ const int numberOfChannels = numLeds * 3; // Total number of DMX channels you wa
 #define ANALOG_PIN A1   
 #define NUM_SAMPLES 10                    // Number of readings to average                  
 #define DEFAULT_BRIGHTNESS 150            // Brightness for init, battery status, wifi status LED
+// #define VOLTAGE
 CRGB leds[numLeds];
 
 // Artnet settings
@@ -28,28 +26,87 @@ bool sendFrame = 1;
 int previousDataLength = 0;
 bool connection_state = false;
 
-void scan_wifi();
+// Function prototypes
+void scan_wifi(void);
+bool connect_wifi(void);
+float read_calibrated_voltage(void);
+void on_dmx_frame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t *data);
+void print_connection_state(void);
 
-float readCalibratedVoltage() 
+void setup()
 {
-    uint32_t adc_raw = 0;
+  Serial.begin(115200);
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, numLeds);
+  FastLED.setBrightness(DEFAULT_BRIGHTNESS);
+  // Initialize ADC Calibration
+  #ifdef VOLTAGE
+  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
+  float voltage = read_calibrated_voltage() * 2;  
+  Serial.print("Calibrated Voltage: ");
+  Serial.print(voltage, 3);
+  Serial.println(" V");
+  if(voltage < 3.0)
+  {
+    voltage = 3.0;
+  }
+  float percentage = (voltage - 3.0) / 1.2;
+  Serial.print("Percentage: ");
+  Serial.print(percentage, 3);
+  Serial.println(" %");
+  int num_leds_battery = percentage * numLeds;
+  
+  // Load light saber to percentage of battery
+  for(int i = 0; i < num_leds_battery; i++)
+  {
+    leds[i] = CRGB(209, 134, 0); // Orange
+    FastLED.show();
+    delay(50);
+  }
 
-    // Take multiple readings and average
-    for (int i = 0; i < NUM_SAMPLES; i++) {
-        adc_raw += analogRead(ANALOG_PIN);
-        delay(5);
-    }
-    adc_raw /= NUM_SAMPLES;
+  delay(3000);
+  
+  // Remove light saber to zero from percentage start point
+  for(int i = num_leds_battery; i > 0; i--)
+  {
+    leds[i] = CRGB(0, 0, 0); // Black
+    FastLED.show();
+    delay(30);
+  }
+  #endif
 
-    // Convert ADC raw value to voltage using ESP32 calibration
-    uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_raw, &adc_chars);
-    return voltage / 1000.0; // Convert mV to V
+  // Switch on light
+  leds[0] = CRGB(209, 134, 0); // Orange
+  FastLED.show();
+
+  // Scan for WiFi networks
+  connection_state = connect_wifi();
+  
+  if (connection_state)
+  {
+    leds[0] = CRGB(0, 255, 0); // Green
+    FastLED.show();
+  }
+  else
+  {
+    leds[0] = CRGB(255, 0, 0); // Red
+    FastLED.show();
+  }
+
+  artnet.begin();
+  // on_dmx_frame will execute every time a packet is received by the ESP32
+  artnet.setArtDmxCallback(on_dmx_frame);
+}
+
+void loop()
+{
+  artnet.read();
+  print_connection_state();
 }
 
 // connect to wifi – returns true if successful or false if not
-boolean ConnectWifi(void)
+bool connect_wifi(void)
 {
-  boolean state = true;
+  bool state = true;
   int i = 0;
 
   WiFi.mode(WIFI_STA); // Ensure it's in station mode
@@ -104,116 +161,6 @@ boolean ConnectWifi(void)
   return state;
 }
 
-void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t *data)
-{
-  sendFrame = 1;
-  // set brightness of the whole strip
-  if (universe == 15)
-  {
-    FastLED.setBrightness(data[0]);
-  }
-  // read universe and put into the right part of the display buffer
-  int led = 0;
-  for (int i = 0; i < length / 3; i++)
-  {
-    led = i + (universe - startUniverse) * (previousDataLength / 3);
-    if (led < numLeds)
-    {
-      leds[led] = CRGB(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
-      // printf("LED nr: %d R:%d G:%d B:%d\n", led, data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
-    }
-  }
-  previousDataLength = length;
-  // Serial.print("on DMX Frame: ");
-  // Serial.print(millis());
-  // printf(" Universe: %d previousDataLength = %d led = %d\n",universe, previousDataLength, led);
-  FastLED.show();
-}
-
-void setup()
-{
-  // Initialize ADC Calibration
-  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
-  Serial.begin(115200);
-  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, numLeds);
-  FastLED.setBrightness(DEFAULT_BRIGHTNESS);
-  float voltage = readCalibratedVoltage() * 2;  
-  Serial.print("Calibrated Voltage: ");
-  Serial.print(voltage, 3);
-  Serial.println(" V");
-  if(voltage < 3.0)
-  {
-    voltage = 3.0;
-  }
-  float percentage = (voltage - 3.0) / 1.2;
-  Serial.print("Percentage: ");
-  Serial.print(percentage, 3);
-  Serial.println(" %");
-  int num_leds_battery = percentage * numLeds;
-  
-  // Load light saber to percentage of battery
-  for(int i = 0; i < num_leds_battery; i++)
-  {
-    leds[i] = CRGB(209, 134, 0); // Orange
-    FastLED.show();
-    delay(50);
-  }
-
-  delay(3000);
-  
-  // Remove light saber to zero from percentage start point
-  for(int i = num_leds_battery; i > 0; i--)
-  {
-    leds[i] = CRGB(0, 0, 0); // Black
-    FastLED.show();
-    delay(30);
-  }
-
-  connection_state = ConnectWifi();
-  // Switch on light
-  if (connection_state)
-  {
-    leds[0] = CRGB(0, 255, 0); // Green
-    FastLED.show();
-  }
-  else
-  {
-    leds[0] = CRGB(255, 0, 0); // Red
-    FastLED.show();
-  }
-
-  artnet.begin();
-  FastLED.setBrightness(100);
-  // onDmxFrame will execute every time a packet is received by the ESP32
-  artnet.setArtDmxCallback(onDmxFrame);
-}
-
-void print_connection_state()
-{
-  static int time_stamp = millis();
-  if (millis() > (time_stamp + 5000))
-  {
-    Serial.print("Connection state: ");
-    if (connection_state)
-    {
-      Serial.print("TRUE - ");
-      Serial.print("IP address: ");
-      Serial.println(WiFi.localIP());
-    }
-    else
-    {
-      Serial.println("FALSE");
-    }
-    time_stamp = millis();
-  }
-}
-
-void loop()
-{
-  artnet.read();
-  print_connection_state();
-}
-
 void scan_wifi()
 {
   WiFi.mode(WIFI_STA);
@@ -238,4 +185,66 @@ void scan_wifi()
   }
 
   Serial.println();
+}
+
+float read_calibrated_voltage() 
+{
+    uint32_t adc_raw = 0;
+
+    // Take multiple readings and average
+    for (int i = 0; i < NUM_SAMPLES; i++) {
+        adc_raw += analogRead(ANALOG_PIN);
+        delay(5);
+    }
+    adc_raw /= NUM_SAMPLES;
+
+    // Convert ADC raw value to voltage using ESP32 calibration
+    uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_raw, &adc_chars);
+    return voltage / 1000.0; // Convert mV to V
+}
+
+void on_dmx_frame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t *data)
+{
+  sendFrame = 1;
+  // set brightness of the whole strip
+  if (universe == 15)
+  {
+    FastLED.setBrightness(data[0]); // has no impact
+  }
+  // read universe and put into the right part of the display buffer
+  int led = 0;
+  for (int i = 0; i < length / 3; i++)
+  {
+    led = i + (universe - startUniverse) * (previousDataLength / 3);
+    if (led < numLeds)
+    {
+      leds[led] = CRGB(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
+      // printf("LED nr: %d R:%d G:%d B:%d\n", led, data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
+    }
+  }
+  previousDataLength = length;
+  // Serial.print("on DMX Frame: ");
+  // Serial.print(millis());
+  // printf(" Universe: %d previousDataLength = %d led = %d\n",universe, previousDataLength, led);
+  FastLED.show();
+}
+
+void print_connection_state()
+{
+  static int time_stamp = millis();
+  if (millis() > (time_stamp + 5000))
+  {
+    Serial.print("Connection state: ");
+    if (connection_state)
+    {
+      Serial.print("TRUE - ");
+      Serial.print("IP address: ");
+      Serial.println(WiFi.localIP());
+    }
+    else
+    {
+      Serial.println("FALSE");
+    }
+    time_stamp = millis();
+  }
 }
